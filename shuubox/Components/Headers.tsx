aa41c66fb673e7f895c151c9fe670f45b6bdda31
Gabriel <gabrielp2638@gmail.com>
2025-10-25 03:45:47 -0400

    Added Frontpage

---
+++ shuubox/Components/Headers.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Shuubox from '@/Icons/shuubox.svg'
+
+
+function Headers() {
+  return (
+   
+    <div className='w-full'>
+      <div className='flex justify-between items-center p-4'>
+        <img src= '@/Icons/shuubox.svg'
+         alt='logo'/>
+      <ul className='flex space-x-4'>
+          <li><button type='button' className='text-white
+          bg-[#7E7E7E]
+        rounded-2xl
+        p-1.5
+        px-8'
+        > Login </button></li>
+        <li><button type='button' className='text-white
+        bg-[#7E7E7E]
+        rounded-2xl
+        p-1.5
+        px-6'
+        > Sign Up </button></li>
+      </ul>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Headers
