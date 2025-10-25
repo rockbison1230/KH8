d7ddd19d24c86aa247df47bb07696932ce4672df
sopgeo <sophiageorgieva@knights.ucf.edu>
2025-10-25 02:43:54 -0400

    css edit

--- shuubox/app/signup/page.tsx
+++ shuubox/app/signup/page.tsx
@@ -60,7 +60,7 @@ export default function SignUp() {
         </p>
 
         {showEmailForm && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
+          <div className="fixed inset-0 flex items-center justify-center bg-opacity-40 z-50">
           <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-sm relative">
             <button
               onClick={handleClose}
